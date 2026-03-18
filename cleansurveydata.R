de56fcd89a252b64a49573a06bd2856c9aa45ef3
StephanieGutie63 <stephanieg5@cpp.edu>
2026-03-17 19:21:36 -0700

    uppercamel

--- cleansurveydata.R
+++ cleansurveydata.R
@@ -5,5 +5,7 @@ SurveyData <- read_csv("surveydata.csv") #call the csv file to open
 
 head(SurveyData)
 
-SurveyData <- clean_names(SurveyData)
+SurveyData <- clean_names(SurveyData) |>
+              clean_names(case= "upper_camel")
+
 head(SurveyData)
